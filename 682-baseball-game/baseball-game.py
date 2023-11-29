3865f1e73e19b3e6718e7e871e0da28183063ce1
Purushothaman Srikanth <43005013+PurushothamanSrikanth@users.noreply.github.com>
2023-11-29 09:37:25 +0530

    Time: 46 ms (73.11%) | Memory: 16.5 MB (17.45%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,32 @@
+class Solution:
+    def is_number(self, value):
+        return isinstance(value, (int, float))
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+
+        for i in operations:
+            if i == "+":
+                if len(stack) >= 2:
+                    new_val = stack[-1] + stack[-2]
+                    stack.append(new_val)
+            elif i == "D":
+                if len(stack):
+                    temp = 2 * stack[-1]
+                    stack.append(temp)
+            elif i == "C":
+                if len(stack):
+                    stack.pop()
+            else:
+                stack.append(int(i))
+        #print(stack)
+        #print(type(stack))
+
+        return sum(stack)
+
+
+
+                
+
+
+
+        
